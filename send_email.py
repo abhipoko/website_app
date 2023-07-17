87531b0221b85b95903d242c041110c84c6b1deb
abhipoko <abhi8895@gmail.com>
2023-07-17 10:31:15 +0530

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -5,7 +5,7 @@ def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     user_name = "abhishek88.tries@gmail.com"
-    password = "mayqvjeyfyolrccv"
+    password = "PASSWORD"
     receiver = "abhishek88_tries@gmail.com"
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL(host, port, context=context) as server:
